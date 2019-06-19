e32bda59d4a8eb216bb2628798c6eb0213f900aa
unknown <riaz.i3216@gmail.com>
2019-06-19 18:18:21 +0600

    after last

--- validation.js
+++ validation.js
@@ -7,6 +7,7 @@ const patterns = {
     username:/^[a-z][a-z\d]{4,11}$/,
     password:/^[\w@-]{8,20}$/,
     slug:/^[a-z\d-]{8,20}$/,
+    email:/^([a-z\d\.]+)@([a-z\d-]+)\.([a-z]){2,8}(\.[a-z]{2,8})?$/,
 
 };
 
